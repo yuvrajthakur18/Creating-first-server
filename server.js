0ad074eae0099286c8488e82037380b7bd965683
yuvrajthakur18 <ysthakur180401@gmail.com>
2022-12-27 13:26:37 +0530

    installed 3rd party package (nodemon)

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const port = 8081;
 
 http
     .createServer((request, response) => {
+        // callback function
         response.writeHead(200, { "Content-Type": "text/html" });
-        response.write("<h1>Hello, this is from my server</h1>");
+        response.write("<h1>Hello, this is from my server</h1><h2>Server is working ... </h2>");
         response.end();
     })
-    .listen(port, () => {
+    .listen(port, () => {                                 
+        // callback function 
         console.log('NodeJS server started on port ${port}');
     });
 
